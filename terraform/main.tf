92f86fd1be751d8c5d00b20f143d3c285b6f7c77
Marcus Chan <conteur.seismes0u@icloud.com>
2026-04-12 15:38:12 +0200

    Fix GitHub Actions errors

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,24 @@ resource "linode_instance" "stirling-pdf" {
   type            = "g6-nanode-1"
   image           = "linode/ubuntu24.04"
   authorized_keys = [var.ssh_public_key]
+
+  metadata {
+    user_data = base64encode(<<-EOF
+      #cloud-config
+      users:
+        - name: marcus
+          groups: sudo
+          shell: /bin/bash
+          sudo: ['ALL=(ALL) NOPASSWD:ALL']
+          ssh_authorized_keys:
+            - ${var.ssh_public_key}
+      
+      runcmd:
+        - sed -i 's/^#*PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+        - systemctl restart ssh
+    EOF
+    )
+  }
 }
 
 resource "local_file" "ansible_inventory" {
