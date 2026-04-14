c9e8cfdb16247ef03948e292bd7f75a86f25b1a0
Marcus Chan <68298575+itsmarcuschan@users.noreply.github.com>
2026-04-15 00:00:30 +0200

    Add test instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,3 +53,29 @@ resource "linode_instance" "stirling-pdf-dev" {
     )
   }
 }
+
+resource "linode_instance" "stirling-pdf-test" {
+  label           = "stirling-pdf-test"
+  region          = "fr-par"
+  type            = "g6-nanode-1"
+  image           = "linode/ubuntu24.04"
+  authorized_keys = [var.ssh_public_key]
+
+  metadata {
+    user_data = base64encode(<<-EOF
+      #cloud-config
+      users:
+        - name: ${var.server_user}
+          groups: sudo
+          shell: /bin/bash
+          sudo: ['ALL=(ALL) NOPASSWD:ALL']
+          ssh_authorized_keys:
+            - ${var.ssh_public_key}
+
+      runcmd:
+        - sed -i 's/^#*PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+        - systemctl restart ssh
+    EOF
+    )
+  }
+}
